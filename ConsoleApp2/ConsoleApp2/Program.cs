a1e4ed376c106ca32368b6909590182e487f3b7d
vanchBim <stecenkoivan1995@gmail.com>
2021-11-03 21:09:55 +0300

    add lesson2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветсвует усреднитель температуры!))\n" + "Введите минимальную температуру за сутки:");
+            string min = Console.ReadLine();
+            int num;
+            bool isNum = int.TryParse(min, out num);
+            if (isNum)
+                Console.WriteLine("Температура записана");
+            else
+            {
+                Console.WriteLine("Введите температуру цифрами!");
+                return;
+            }
+            Console.WriteLine("Введите максимальную температуру за сутки:");
+            string max = Console.ReadLine();
+            int num1;
+            bool isNum1 = int.TryParse(max, out num1);
+            if (isNum)
+                Console.WriteLine("Температура записана");
+            else
+                Console.WriteLine("Введите температуру цифрами!");
+            double med = (num + num1) / 2.0;
+
+            Console.WriteLine($"Средняя температура за сутки:{med}");
+        }
+    }
+}
