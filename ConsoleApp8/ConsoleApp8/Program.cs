eb37d3fda055529af956912b018c5c5b3048dbd0
vanchBim <stecenkoivan1995@gmail.com>
2021-11-07 15:06:21 +0300

    add lesson 3

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку :");
+            string str = Console.ReadLine();
+            Console.WriteLine("Строка наоборот :");
+            for (int i = str.Length-1; i >= 0; i--)
+                Console.Write($"{str[i]}");
+        }
+    }
+}
