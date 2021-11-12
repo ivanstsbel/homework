b53c1bd5b73e32fec70fe9f989284900e849f786
vanchBim <stecenkoivan1995@gmail.com>
2021-11-12 19:56:43 +0300

    add lesoon 4

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер месяца :");
+            string pn = Console.ReadLine();
+            int number;
+            bool tr = int.TryParse(pn, out number);
+            if (!(tr == true && number < 13 && number > 0))
+                Console.WriteLine("Ошибка: введите число от 1 до 12!");
+            else Console.WriteLine($"{timeyear(vge(number))}") ;
+            
+        }
+        static string vge(int n)
+        {
+            string vrr = null;
+            switch(n)
+                {
+                case 1:
+                    vrr = "winter";
+                  break;
+                case 2:
+                    vrr = "winter";
+                    break;
+                case 3:
+                    vrr = "Spring";
+                    break;
+                case 4:
+                    vrr = "Spring";
+                    break;
+                case 5:
+                    vrr = "Spring";
+                    break;
+                case 6:
+                    vrr = "Summer";
+                    break;
+                case 7:
+                    vrr = "Summer";
+                    break;
+                case 8:
+                    vrr = "Summeк";
+                    break;
+                case 9:
+                    vrr = "Autumn";
+                    break;
+                case   10:
+                    vrr = "Autumn";
+                    break;
+                case 11:
+                    vrr = "Autumn";
+                    break;
+                case 12:
+                    vrr = "winter";
+                    break;
+
+            }
+            return vrr;
+      
+        } 
+        static string timeyear (string x)
+        {
+            if( x == "winter")
+                return "Зима";
+            if (x == "Spring")
+                return "весна";
+            if (x == "Summer")
+                return "лето";
+            if (x == "Autumn")
+                return "осень";
+            else return null;
+        }
+
+    }
+
+    }
+
+    
+        
+
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fName = { "Алексей", "Петр", "Сергей", "Владимир"};
+            string[] lName = { "Богомолов", "Кузнецов", "Медведев", "Иванов"};
+            string[] pName = { "Иванович", "Александрович", "Владимирович", "Евгеньевич"};
+
+            Random rnd = new Random();
+          
+
+            for (int i = 0; i <= 3; i++)
+            {
+                int value = rnd.Next(0, 4);
+                int value2 = rnd.Next(0, 4);
+                int value3 = rnd.Next(0, 4);
+                Console.WriteLine($"{GetFullName(fName[value], lName[value2], pName[value3])}");
+            }
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string FullName = ($"{lastName} {firstName}  {patronymic}");
+            return FullName;
+        }
+    }
+}
