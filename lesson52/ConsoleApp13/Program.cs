b58e9bbebf1bba9d2cd0db0141cbac9fd44a6362
vanchBim <stecenkoivan1995@gmail.com>
2021-11-15 22:32:24 +0300

    add lesson 5

---
+++ lesson52/ConsoleApp13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime();
+            string filename = "startup.txt";
+            string time = ($"{DateTime.Now}");
+            File.AppendAllText(filename, time); // записываем в файл строк
+            Console.WriteLine($"Время и дата {DateTime.Now} записаны в файл");
+        }
+    }
+}
---
+++ lesson53/ConsoleApp14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа от 0 до 255");
+            string numbStr = Console.ReadLine();
+            string[] numbStrArr = numbStr.Split();
+            byte[] numbArr = new byte[numbStrArr.Length];
+            for (int i = 0; i < numbStrArr.Length; i++)
+            {
+                Convert.ToByte(numbStrArr[i]);
+                numbArr[i] = Convert.ToByte(numbStrArr[i]);
+
+            }
+            
+            File.WriteAllBytes("bytes.bin", numbArr);
+        }
+    }
+}
